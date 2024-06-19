f78f81f619dc9a8621259133b576b8689c0a10e1
Akash Anumarlapudi <akashjanaki98@gmail.com>
2024-06-19 13:14:38 -0500

    Add psrqpy to dependencies

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires=">=3.9, <4",
+    python_requires=">=3.8",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
@@ -135,6 +135,7 @@ setup(
         "astroquery>=0.4.7",
         "svgpath2mpl",
         "bs4",
+        "psrqpy",
     ],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
